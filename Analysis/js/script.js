d62a39053203887ac210559d17e59a733bf67207
dianamin <97.diana@gmail.com>
2014-06-04 01:21:04 +0300

    First Version

---
+++ Analysis/js/script.js
@@ -0,0 +1,11 @@
+﻿/// <reference group="Dedicated Worker" />
+
+onmessage = function (event) {
+
+}
+
+var current;
+var nav = function (x) {
+    document.getELementById("m" + x).innerHTML = "yay";
+}
+
