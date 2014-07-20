61685ce036267fd51f0bd389cf2626147e036b50
dianamin <97.diana@gmail.com>
2014-07-20 14:13:53 +0300

    User :D:D:D

--- Analysis/bin/Debug/AppX/js/register.js
+++ Analysis/bin/Debug/AppX/js/register.js
@@ -14,12 +14,21 @@ Windows.System.UserProfile.UserInformation.getDisplayNameAsync().done(function (
 });
 console.log(x);
 
+var imgURL;
+
+var image = Windows.System.UserProfile.UserInformation.getAccountPicture(Windows.System.UserProfile.AccountPictureKind.smallImage);
+if (image) {
+    imgURL = URL.createObjectURL(image, { oneTimeOnly: true });
+    WinJS.log && WinJS.log("Small image path= " + image.path, "sample", "status");
+}
+console.log(image.path);
 
 var app = angular.module('analysis', []);
 app.controller('User', ['$scope', function ($scope) {
     Windows.System.UserProfile.UserInformation.getDisplayNameAsync().done(function (data) {
         $scope.username = data;
         console.log($scope.username);
+        $scope.img = image.path;
     });
 }]);
 
